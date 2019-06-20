249efe57be3865860133fc30e25729f04044eb4e
kidfang <50686062+kidfang@users.noreply.github.com>
2019-06-20 10:01:52 +0800

    Update ipmi_sdr_count.sh

--- ipmi/ipmi_sdr_count.sh
+++ ipmi/ipmi_sdr_count.sh
@@ -10,23 +10,22 @@ next=0
 
 for (( i=1; i<=$num; i=i+1 ));
 	do	
-		typeset -u next
 		DEX_to_HEX=$(echo "ibase=10;obase=16;$i"|bc)
 		check=$(ipmitool -H ${bmcip} -U admin -P password -I lanplus raw 0x0a 0x22)
 		check_a=$(echo $check | cut -f 1 -d " ")
 		check_b=$(echo $check | cut -f 2 -d " ")
-		if [ $next = 0 ]; then
+
+		if [ $next == 0 ]; then
 			next=$i
 		fi
 
 		SDR_con=$(ipmitool -H ${bmcip} -U admin -P password -I lanplus raw 0x0a 0x23 0x$check_a 0x$check_b 0x$next 0x00 0x00 0xff)
 
-#		typeset -u Record_length
 		Record_type=$(echo $SDR_con | cut -f 6 -d " ")
-#		Record_length=$(echo $SDR_con | cut -f 7 -d " ")
+		Sensor_Number=$(echo $SDR_con | cut -f 10 -d " ")
 		next=$(echo $SDR_con | cut -f 1 -d " ")
 
-		case ${Record_type} in
+		case ${Record_type} in    # Set the start byte for ID string ASCII transfer
 				"01")
 				Start=51
 				;;
@@ -52,12 +51,11 @@ for (( i=1; i<=$num; i=i+1 ));
                                 Start=19
                                 ;;
 		esac
-		
-#		HEX_to_DEX=$(echo "obase=10;ibase=16;$Record_length"|bc)
+
 		Sensor_name_HEX=$(echo $SDR_con | cut -f $Start-100 -d " ")
 		Sensor_name_ASCII=$(echo $Sensor_name_HEX | xxd -r -ps)
 
-		echo "$i    $DEX_to_HEX    $next     $Sensor_name_ASCII   Record_type: $Record_type"
+		echo "$i     $DEX_to_HEX     $next     $Sensor_Number    $Sensor_name_ASCII           Record_type: $Record_type"
 	done
 
 
@@ -74,5 +72,3 @@ for (( i=1; i<=$num; i=i+1 ));
 #13 => 
 #14 => 
 #C0 =>
-
-
