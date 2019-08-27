2770f2d3711c500e7e8ba373444db16cd25044f3
kidfang <50686062+kidfang@users.noreply.github.com>
2019-08-27 20:31:11 +0800

    Update mlnx_ib_bw.sh

--- iperf/mlnx_ib_bw.sh
+++ iperf/mlnx_ib_bw.sh
@@ -1,10 +1,10 @@
 
 #RX
-taskset -c $cpu_core ib_write_bw -a -b mlx5_0 --report_gbits -p 18515
+taskset -c $cpu_core ib_write_bw -a -b -d mlx5_0 --report_gbits -p 18515
 
-taskset -c $cpu_core ib_write_bw -a -b mlx5_1 --report_gbits -p 18516
+taskset -c $cpu_core ib_write_bw -a -b -d mlx5_1 --report_gbits -p 18516
 
 #TX
-taskset -c $cpu_core ib_write_bw -a -b mlx5_0 --report_gbits $RX_ip -p 18515
+taskset -c $cpu_core ib_write_bw -a -b -d mlx5_0 --report_gbits $RX_ip -p 18515
 
-taskset -c $cpu_core ib_write_bw -a -b mlx5_1 --report_gbits $RX_ip -p 18516
+taskset -c $cpu_core ib_write_bw -a -b -d mlx5_1 --report_gbits $RX_ip -p 18516
