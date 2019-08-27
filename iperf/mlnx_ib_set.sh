83fd0942df667585c8a48db0a58b272d209fc08e
kidfang <50686062+kidfang@users.noreply.github.com>
2019-08-27 20:25:09 +0800

    Add files via upload

---
+++ iperf/mlnx_ib_set.sh
@@ -0,0 +1,21 @@
+eth_a=$1
+ip_a=$2
+
+ifconfig "$eth_a" "$ip_a" up
+
+#lan_num=$(ibv_devinfo | grep -i vendor_part_id | wc -l)
+hca_id=$(mst status -v | grep -i $eth_a | awk '{print $4}')
+lan_num=$(mst status -v | grep -i $eth_a | awk '{print $4}' | cut -f 2 -d "_")
+
+if [ $lan_num = 0 ]; then
+
+	# ib0 link ready
+	/etc/init.d/opensmd start
+else
+	# ib1 link ready
+	port_guid=$(ibstat "$hca_id" | grep -i "Port GUID" | awk '{print $3}'| sed -n '1p')
+	opensm -g $port_guid
+
+fi
+
+ibdev2netdev
