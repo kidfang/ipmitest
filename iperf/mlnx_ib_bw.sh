b718d157e818c33ee7bcf5a6f9dbfa0c267b8f4b
kidfang <50686062+kidfang@users.noreply.github.com>
2019-08-28 13:45:51 +0800

    Update mlnx_ib_bw.sh

--- iperf/mlnx_ib_bw.sh
+++ iperf/mlnx_ib_bw.sh
@@ -1,10 +1,30 @@
+#For 2port ib lan card
 
-#RX
-taskset -c $cpu_core ib_write_bw -a -b -d mlx5_0 --report_gbits -p 18515
+test_mode=$1  #SOP or RDMA
+test_sut=$2   #RX or TX
+test_vid=$3   #Ex. mlx5
+test_numa=$4  #Numa node
+test_ip=$5    #RX_ip
+cpu_core=$6   #cpu core
 
-taskset -c $cpu_core ib_write_bw -a -b -d mlx5_1 --report_gbits -p 18516
+if [ $test_mode = SOP ]; then
 
-#TX
-taskset -c $cpu_core ib_write_bw -a -b -d mlx5_0 --report_gbits $RX_ip -p 18515
-
-taskset -c $cpu_core ib_write_bw -a -b -d mlx5_1 --report_gbits $RX_ip -p 18516
+  if [ $test_sut = RX ]; then
+    #RX
+    taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_0 --report_gbits -p 18516 & taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_1 --report_gbits -p 18517
+  else
+    #TX
+    taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_0 --report_gbits $test_ip -p 18516 & taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_1 --report_gbits $test_ip -p 18517
+  fi
+  
+else
+  
+  if [ $test_sut = RX ]; then
+    #RX
+    numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_0 -D 66 --report_gbits --cpu_util -p 18516 & numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_1 -D 66 --report_gbits --cpu_util -p 18517
+  else
+    #TX
+    numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_0 -D 66 --report_gbits --cpu_util $test_ip -p 18516 & numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_1 -D 66 --report_gbits --cpu_util $test_ip -p 18517
+  fi
+  
+fi
