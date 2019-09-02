dbd1fd9ad9bde279aa71b6ccbe9603aaff8acbcb
kidfang <50686062+kidfang@users.noreply.github.com>
2019-09-02 15:58:31 +0800

    Update mlnx_ib_bw.sh

--- iperf/mlnx_ib_bw.sh
+++ iperf/mlnx_ib_bw.sh
@@ -4,8 +4,9 @@ test_mode=$1  #SOP or RDMA
 test_sut=$2   #RX or TX
 test_vid=$3   #Ex. mlx5
 test_numa=$4  #Numa node
-test_ip=$5    #RX_ip
-cpu_core=$6   #cpu core
+cpu_core=$5   #cpu core
+test_ip_0=$6    #RX_ip_0
+test_ip_1=$7    #RX_ip_1
 
 if [ $test_mode = SOP ]; then
 
@@ -14,7 +15,7 @@ if [ $test_mode = SOP ]; then
     taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_0 --report_gbits -p 18516 & taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_1 --report_gbits -p 18517
   else
     #TX
-    taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_0 --report_gbits $test_ip -p 18516 & taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_1 --report_gbits $test_ip -p 18517
+    taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_0 --report_gbits $test_ip_0 -p 18516 & taskset -c $cpu_core ib_write_bw -a -b -d "$test_vid"_1 --report_gbits $test_ip_1 -p 18517
   fi
   
 else
@@ -24,7 +25,7 @@ else
     numactl -N $test_numa -l ib_write_bw -t 256 -b -d "$test_vid"_0 -D 66 --report_gbits --cpu_util -p 18516 & numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_1 -D 66 --report_gbits --cpu_util -p 18517
   else
     #TX
-    numactl -N $test_numa -l ib_write_bw -t 256 -b -d "$test_vid"_0 -D 66 --report_gbits --cpu_util $test_ip -p 18516 & numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_1 -D 66 --report_gbits --cpu_util $test_ip -p 18517
+    numactl -N $test_numa -l ib_write_bw -t 256 -b -d "$test_vid"_0 -D 66 --report_gbits --cpu_util $test_ip_0 -p 18516 & numactl -N $test_numa -l ib_read_bw -t 256 -b -d "$test_vid"_1 -D 66 --report_gbits --cpu_util $test_ip_1 -p 18517
   fi
   
 fi
