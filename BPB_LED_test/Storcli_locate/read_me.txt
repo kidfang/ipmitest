abd23afafada3dab73e8f730dadc3cbf1ede50db
kidfang <50686062+kidfang@users.noreply.github.com>
2019-09-20 14:21:23 +0800

    Add files via upload

---
+++ BPB_LED_test/Storcli_locate/read_me.txt
@@ -0,0 +1,12 @@
+
+
+
+1. Installing debian Package
+	sudo dpkg -i storcli_1.0_all.deb
+
+2. verifying , if the package is installed successfully or not.
+   dpkg -l | grep -i storcli
+
+3. Run the commands    with super user as below.
+	sudo /opt/MegaRAID/storcli/storcli adpcount
+
---
+++ BPB_LED_test/Storcli_locate/storcli_locate.sh
@@ -0,0 +1,14 @@
+for (( i=0; i<=1; i=i+1 ));
+	do
+		echo -e "\nInput EID number:\n"
+		read eid_num
+	for (( k=0; k<=29; k=k+1 ));
+		do
+		/opt/MegaRAID/storcli/storcli64 /c$i /e"$eid_num" /s$k start locate
+
+		echo -e "\n"
+		read -n 1 -p "Check locate correct or not ..."
+
+		/opt/MegaRAID/storcli/storcli64 /c$i /e"$eid_num" /s$k stop locate
+		done
+	done
