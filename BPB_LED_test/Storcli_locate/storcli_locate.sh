ef0519ea2c6924ffda515ac19064cc35f0510f19
kidfang <50686062+kidfang@users.noreply.github.com>
2019-09-20 14:24:42 +0800

    Update storcli_locate.sh

--- BPB_LED_test/Storcli_locate/storcli_locate.sh
+++ BPB_LED_test/Storcli_locate/storcli_locate.sh
@@ -2,13 +2,16 @@ for (( i=0; i<=1; i=i+1 ));
 	do
 		echo -e "\nInput EID number:\n"
 		read eid_num
+		
 	for (( k=0; k<=29; k=k+1 ));
 		do
+		
 		/opt/MegaRAID/storcli/storcli64 /c$i /e"$eid_num" /s$k start locate
 
 		echo -e "\n"
 		read -n 1 -p "Check locate correct or not ..."
 
 		/opt/MegaRAID/storcli/storcli64 /c$i /e"$eid_num" /s$k stop locate
+		
 		done
 	done
