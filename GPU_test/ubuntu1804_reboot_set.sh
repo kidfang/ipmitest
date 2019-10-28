3efa8dbc837839ecd71239618d6351e0ef365769
kidfang <50686062+kidfang@users.noreply.github.com>
2019-10-28 13:27:39 +0800

    Update ubuntu1804_reboot_set.sh

--- GPU_test/ubuntu1804_reboot_set.sh
+++ GPU_test/ubuntu1804_reboot_set.sh
@@ -1,15 +1,20 @@
+#link rc-local.service
 ln -fs /lib/systemd/system/rc-local.service /etc/systemd/system/rc-local.service
 
+#edit rc-local.service
 echo "
 [Install]
 WantedBy=multi-user.target
 Alias=rc-local.service
 " >> /etc/systemd/system/rc-local.service
 
+#create rc.local
 touch /etc/rc.local
 
+#add permission
 chmod +x /etc/rc.local
 
+#set reboot script into rc.local
 echo "#!/bin/bash
 /home/smbuser/reboot.sh
 " > /etc/rc.local
