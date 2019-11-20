001e66939e058feee2e2983b466952e9f125ebee
kidfang <50686062+kidfang@users.noreply.github.com>
2019-11-20 14:08:01 +0800

    Add files via upload

---
+++ GPU_test/CMake_install/cmake3_12_2_install.sh
@@ -0,0 +1,18 @@
+#CMake 3.12.2 Download and install
+
+#Download CMake 3.12.2
+wget https://github.com/Kitware/CMake/archive/v3.12.2.tar.gz
+
+#Unzip file
+tar -zxvf v3.12.2.tar.gz
+
+#Into CMake 3.12.2 folder
+cd CMake-3.12.2
+
+#Start install CMake 3.12.2
+./bootstrap && gmake && gmake install
+
+#Check CMake version
+cmake --version
+
+echo -e "\n\n CMake version should be 3.12.2 \n\n"
