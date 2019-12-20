929352efa558e8a6f2c65625cca4ab60017d5c71
kidfang <50686062+kidfang@users.noreply.github.com>
2019-12-20 10:20:47 +0800

    Update run_gpu_tool.sh

--- run_gpu_tool.sh
+++ run_gpu_tool.sh
@@ -2,10 +2,10 @@
 result_output=$1        # /home/smbuser
 CUDA_path=$2            # /root/NVIDIA_CUDA-10.1_Samples
 
-echo -e "\nPlease input the test type (set_tool/basic/p2p/bw): "
+echo -e "\nPlease input the test type (nv_set_tool/basic/p2p/bw): "
 read test_type
 
-Install_tool()
+NV_Install_tool()
 
 {
 
@@ -20,23 +20,23 @@ dracut -f
 
 echo -e "\n Install completed, plz reboot system"
 
-
 }
 
 basic_info()
 
 {
 
-nvidia-smi | tee $result_output/nvidia_smi.log
-nvidia-smi -a | tee $result_output/nvidia_smi_a.log
-nvidia-smi -q | tee $result_output/nvidia_smi_q.log
-nvidia-smi -q | grep -i vbios | tee $result_output/nvidia_smi_vbios.log
-lshw -c memory -short | tee  $result_output/mem_info.log
-dmidecode -t memory | tee $result_output/mem.log
-dmidecode -t bios | tee $result_output/bios.log
-lspci | grep -i NVIDIA | tee $result_output/nvidia_gpu_pcie.log
-lspci -tv | tee $result_output/lspci_tv.log
-lscpu | tee $result_output/lscpu.log
+mkdir $result_output/Basic_info
+nvidia-smi | tee $result_output/Basic_info/nvidia_smi.log
+nvidia-smi -a | tee $result_output/Basic_info/nvidia_smi_a.log
+nvidia-smi -q | tee $result_output/Basic_info/nvidia_smi_q.log
+nvidia-smi -q | grep -i vbios | tee $result_output/Basic_info/nvidia_smi_vbios.log
+lshw -c memory -short | tee  $result_output/Basic_info/mem_info.log
+dmidecode -t memory | tee $result_output/Basic_info/mem.log
+dmidecode -t bios | tee $result_output/Basic_info/bios.log
+lspci | grep -i NVIDIA | tee $result_output/Basic_info/nvidia_gpu_pcie.log
+lspci -tv | tee $result_output/Basic_info/lspci_tv.log
+lscpu | tee $result_output/Basic_info/lscpu.log
 
 }
 
@@ -53,15 +53,22 @@ bw_test()
 
 {
 
-for i in {0..7} ; do $CUDA_path/1_Utilities/bandwidthTest/bandwidthTest --device=$i >> $result_output/GPU_bw_all.txt ; done
+GPU_num=$( nvidia-smi -q | grep -i vbios | wc -l )
+
+for (( i=0; i<$GPU_num; i=i+1 ));
+  do
+  $CUDA_path/1_Utilities/bandwidthTest/bandwidthTest --device=$i | tee -a $result_output/GPU_bw_all.txt 
+  done
+  
+$CUDA_path/1_Utilities/bandwidthTest/bandwidthTest --device=all | tee $result_output/GPU_bw_all_onetime.txt 
 
 }
 
 case ${test_type} in
 
-	"set_tool")
+	"nv_set_tool")
 		echo "Start to set and install tool ... "
-		Install_tool 1
+		NV_Install_tool 1
 		;;
 	"p2p")
 		echo "Start to test p2p ... "
