093cc9df7d8aa8cd47f2fa22265ffc65f8107ec7
kidfang <50686062+kidfang@users.noreply.github.com>
2020-01-17 20:12:49 +0800

    Add files via upload

---
+++ GPU_stress_10times.sh
@@ -0,0 +1,13 @@
+
+for (( i=1; i<=10; i=i+1 ));
+ 	do
+		/home/test/gpu-burn/gpu_burn 300 | tee /home/test/Stress_result_gpuburn/gpuburn_test_"$i".log
+		sleep 10
+ 	done
+
+for (( j=1; j<=10; j=j+1 ));
+        do
+		stress-ng --cpu `nproc` --vm `nproc` --vm-bytes 90% --io `nproc` --hdd `nproc` --hdd-bytes 1g --timeout 300 | tee /home/test/Stress_result_stress-ng/stress_ng_test_"$i".log
+		sleep 10
+        done
+
