c6aa2c106792249d09b344ade5e4eab6eb4d4d96
kidfang <50686062+kidfang@users.noreply.github.com>
2020-01-20 13:25:05 +0800

    Update GPU_stress_10times.sh

--- GPU_stress_10times.sh
+++ GPU_stress_10times.sh
@@ -3,13 +3,9 @@ mkdir /home/test/Stress_result_stress-ng >/dev/null 2>&1
 
 for (( i=1; i<=10; i=i+1 ));
  	do
-		/home/test/gpu-burn/gpu_burn 300 | tee /home/test/Stress_result_gpuburn/gpuburn_test_"$i".log
-		sleep 10
+		cd /home/test/gpu-burn
+		./gpu_burn 300 | tee /home/test/Stress_result_gpuburn/gpuburn_test_"$i".log & stress-ng --cpu `nproc` --vm `nproc` --vm-bytes 90% --io `nproc` --hdd `nproc` --hdd-bytes 1g --timeout 300 2>&1 | tee /home/test/Stress_result_stress-ng/stress_ng_test_"$i".log
+		sleep 20
+		date | tee -a /home/test/Stress_result_gpuburn/gpuburn_test_"$i".log
+                date | tee -a /home/test/Stress_result_stress-ng/stress_ng_test_"$i".log
  	done
-
-for (( j=1; j<=10; j=j+1 ));
-        do
-		stress-ng --cpu `nproc` --vm `nproc` --vm-bytes 90% --io `nproc` --hdd `nproc` --hdd-bytes 1g --timeout 300 | tee /home/test/Stress_result_stress-ng/stress_ng_test_"$i".log
-		sleep 10
-        done
-
