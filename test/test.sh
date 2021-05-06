dea4d91751d4ecfa6933ebe41d15bb3510046755
kidfang <50686062+kidfang@users.noreply.github.com>
2021-05-06 15:53:31 +0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -13,3 +13,10 @@ ls non >> test.sh 2>&1
 k=8
 d=$[$k/2]
 echo $d
+
+
+ ipmitool raw 0x2e 0x11 0x0A 0x3C 0x00 0x11
+ 0a 3c 00 23 00 00 00 00 00 22 00 00 00 00 00 00
+ 00
+ 
+ 23 => MB CPLD 22=> BPB CPLD
