056fb35a280691a410d53191d6a0bcef4bad3613
kidfang <50686062+kidfang@users.noreply.github.com>
2021-12-21 10:54:35 +0800

    Update nvme_temp.sh

--- nvme_temp.sh
+++ nvme_temp.sh
@@ -7,8 +7,9 @@ num=$(nvme list | grep nvme | wc -l)
 for (( i=0; i<$num; i=i+1 ));
 do
 
-	Temp=$(nvme smart-log /dev/nvme$i | grep -i temperature | sed -n 1p)
+	#Temp=$(nvme smart-log /dev/nvme$i | grep -i temperature | sed -n 1p)
+	tmp=$(nvme smart-log /dev/nvme"$i"n1 | grep temperature | awk '{print $3 " " $4}')     #> $Path/"$x"_nvme"$i"n1_smart_log_$1_stress.txt
+	Nv_pci=$(ls -l /sys/block/nvme* | grep -i nvme"$i" | sed -n 1p | awk '{print $11}' | cut -f 5 -d "/")
+	echo -e "\n $Nv_pci   nvme"$i"n1   $tmp"
 
-	echo $Temp
 done
-
