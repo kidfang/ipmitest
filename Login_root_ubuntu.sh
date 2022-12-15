8e71e54983a86b96e44680f264c01e7bbd477686
kidfang <50686062+kidfang@users.noreply.github.com>
2022-12-15 10:13:42 +0800

    Update Login_root_ubuntu.sh

--- Login_root_ubuntu.sh
+++ Login_root_ubuntu.sh
@@ -1,3 +1,6 @@
 passwd root
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
+# Auto login
+# sed -i 's/ExecStart=-/sbin/agetty -o '-p -- \u' --noclear %I $TERM/ExecStart=-/sbin/agetty --autologin root --noclear %I $TERM/g' /lib/systemd/system/getty@.service
