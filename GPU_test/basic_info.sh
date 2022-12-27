290df7c063d2db64d7ae5ea0547c7df6c0847f82
kidfang <50686062+kidfang@users.noreply.github.com>
2022-12-27 14:37:08 +0800

    Update basic_info.sh

--- GPU_test/basic_info.sh
+++ GPU_test/basic_info.sh
@@ -8,7 +8,7 @@ nvidia-smi -q | tee $result_output/Basic_info/nvidia_smi_q.txt
 nvidia-smi -q | grep -i vbios | tee $result_output/Basic_info/nvidia_smi_vbios.txt
 nvidia-smi topo -m | tee $result_output/Basic_info/nvidia_smi_topo.txt
 
-nvidia-smi -q | grep -i "GPU 00000000:" | tee -a $result_output/Basic_info/GPU_SN_mapping.txt
+nvidia-smi -q | grep -i "GPU 0000" | tee -a $result_output/Basic_info/GPU_SN_mapping.txt
 nvidia-smi -q | grep -i "Serial" | tee -a $result_output/Basic_info/GPU_SN_mapping.txt
 
 lshw -c memory -short | tee  $result_output/Basic_info/mem_info.txt
