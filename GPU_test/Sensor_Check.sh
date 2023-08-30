060e0edaad4a59831919dbd7f2803a2ebe5c506d
kidfang <50686062+kidfang@users.noreply.github.com>
2023-08-30 13:21:48 +0800

    Update Sensor_Check.sh

--- GPU_test/Sensor_Check.sh
+++ GPU_test/Sensor_Check.sh
@@ -5,9 +5,11 @@ awk '{print $1 " " $3 " " "&"}' $file1 > 1.txt
 awk '{print $3 " " $5}' $file2 > 2.txt
 awk 'NR==FNR{a[i]=$0;i++}NR>FNR{print a[j]" "$0;j++}' 1.txt 2.txt > 3.txt
 
+cat 3.txt | grep -i P12V_S | awk '{print $1}'
+cat 3.txt | grep -i P12V_S | awk '{print $2 " " $3 " " $4 " " $5 " " "(idle Status & loading Status)" }'
+
 cat 3.txt | grep -i gpu | awk '{print $1}'
 cat 3.txt | grep -i gpu | awk '{print $2 " " $3 " " $4 " " $5 " " "(idle Status & loading Status)" }'
-
 
 ### 1.txt , Catch Sensor name and idle sensors value add & at all the last word
 ### 2.txt , Catch stress sensors value 
